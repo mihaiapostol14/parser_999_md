532b0eadb8531f3e74e4492005d568eafc7e1700
Mihai Apostol <mihaiapostol153@gmail.com>
2025-05-12 11:29:09 +0300

    Change container name with item info

--- parser_item_info.py
+++ parser_item_info.py
@@ -4,7 +4,10 @@ from selenium.webdriver.firefox.service import Service
 from selenium.common.exceptions import NoSuchElementException
 
 from config import USER_AGENT
-from helper import Helper
+from helper import (
+    Helper,
+    ElementChecker
+)
 
 
 class ParserItemInfo(Helper):
@@ -23,6 +26,7 @@ class ParserItemInfo(Helper):
                                         options=self.options)  # Create a new instance of the Firefox WebDriver with the specified options
 
         self.source_file = source_file
+        self.checker = ElementChecker(driver=self.driver)
 
         self.remove_duplicate(
             default=f"{self.source_file.split("/")[0]}/cars_Unsorted_link.txt",
@@ -31,59 +35,6 @@ class ParserItemInfo(Helper):
 
         self.iter_by_item()
 
-    def xpath_exists(self, xpath):
-        """Checks if an element with the given XPath exists on the page.
-
-        Args:
-            xpath (str): The XPath of the element to check.
-
-        Returns:
-            bool: True if the element exists, False otherwise.
-        """
-        try:
-            # Attempt to find the element by XPath
-            self.driver.find_element(By.XPATH, xpath)
-            exist = True
-        except NoSuchElementException:
-            # If NoSuchElementException is raised, the element does not exist
-            exist = False
-        return exist
-
-    def id_exists(self, element_id):
-        """Checks if an element with the given ID exists on the page.
-
-        Args:
-            element_id (str): The ID of the element to check.
-
-        Returns:
-            bool: True if the element exists, False otherwise.
-        """
-        try:
-            # Attempt to find the element by XPath
-            self.driver.find_element(By.ID, element_id)
-            exist = True
-        except NoSuchElementException:
-            # If NoSuchElementException is raised, the element does not exist
-            exist = False
-        return exist
-
-    def class_exists(self, class_name):
-        """Checks if an element with the given class name exists on the page.
-
-        Args:
-            class_name (str): The class name of the element to check.
-
-        Returns:
-            bool: True if the element exists, False otherwise.
-        """
-        try:
-            # Attempt to find the element by class name
-            self.driver.find_element(By.CLASS_NAME, class_name)
-            exist = True
-        except NoSuchElementException:
-            # If NoSuchElementException is raised, the element does not exist
-            exist = False
-        return exist
 
     def send_by_url(self, url):
         """
@@ -115,8 +66,8 @@ class ParserItemInfo(Helper):
         self.driver.execute_script("arguments[0].scrollIntoView();", self.driver.find_element(By.CLASS_NAME, 'adPage__content__footer__wrapper'))
 
         try:
-            if self.class_exists(class_name='adPage__content__footer__wrapper'):
-                phone_number = self.driver.find_element(By.CLASS_NAME, 'adPage__content__footer__wrapper').find_element(By.TAG_NAME, 'a').get_attribute('href')
+            if self.checker.class_exists(class_name='styles_footer__sKQxZ'):
+                phone_number = self.driver.find_element(By.CLASS_NAME, 'styles_footer__sKQxZ').find_element(By.TAG_NAME, 'a').get_attribute('href')
                 phone_number = phone_number.replace('tel:', '')
 
                 self.crate_file(
